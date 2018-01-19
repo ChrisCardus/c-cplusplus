f219d6967f3b1c5bb567ba755e9091c16580fef1
Chris Cardus <cxc357@student.bham.ac.uk>
2018-01-19 13:33:38 +0000

    I replaced the temp variable with an array. Not sure why.

--- ex0/fib.c
+++ ex0/fib.c
@@ -1,11 +1,13 @@
-long fib(long m)
+long fibvals[1000];             // Array declaration.
+
+long fib(long m)                // Calculate the mth digit in the Fibonacci sequence.
 {
-    long f1 = 0, f2 = 1, temp;
-    for(int i = 0; i < m; i++)
+    long f1 = 0, f2 = 1;        // Initialise variables f1 & f2 as the first digits in the sequence.
+    for(int i = 0; i < m; i++)  // Create a variable i with value 0. Loop m times. After each loop, add 1 to i.
     {
-        temp = f1 + f2;
-        f1 = f2;
-        f2 = temp;
+        fibvals[i] = f1 + f2;   // The ith digit of the sequence is equal to f1 + f2.
+        f1 = f2;                // Move f1 to the next digit in the sequence.
+        f2 = fibvals[i];        // Move f2 to the next digit in the sequence.
     }
-    return f2;
+    return f2;                  // After we have calculated the whole sequence up until the mth digit, we return the mth digit.
 }
