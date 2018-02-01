96f96e79a9d2dac15a54ccb88e237dd2707cab01
Chris Cardus <cxc357@student.bham.ac.uk>
2018-02-01 01:01:48 +0000

    No progress, try linked lists

--- ex1/freegraph.c
+++ ex1/freegraph.c
@@ -3,19 +3,14 @@
 #include <stdio.h>
 
 void deallocate(struct N *p) {
-    if(p){
-        printf("True\n");
-    } else {
-        printf("False\n");
+
+    while(p) {
+
+        free(p);
+
+
+
+    
     }
-    while (p) {
-        //printf("1");
-        deallocate(p->x);
-        //printf("2");
-        deallocate(p->y);
-        //printf("3");
-        deallocate(p->z);
-        //printf("4");
-    }
-    free(p);
+
 }
