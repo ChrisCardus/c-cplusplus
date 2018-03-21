7e0e89fbd001a123bea383f3c588eecc30d142a2
Chris Cardus <cxc357@student.bham.ac.uk>
2018-03-21 13:41:42 +0000

    Finished ex4.

--- ex4/destructobj.cpp
+++ ex4/destructobj.cpp
@@ -2,3 +2,35 @@
 using namespace std;
 #include "destructobj.h"
 
+Exp::~Exp()
+{
+
+}
+
+Constant::~Constant()
+{
+
+}
+
+Var::~Var()
+{
+
+}
+
+Let::~Let()
+{
+    delete bexp;
+    delete body;
+}
+
+OpApp::~OpApp()
+{
+    ExpList *current = args;
+    while(current)
+    {
+        delete current->head;
+        ExpList *temp = current;
+        current = current->tail;
+        delete temp;
+    }
+}
